aadef87c5ad90285b29a9aa2807f7e9ec8a483a3
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2025-02-22 17:06:50 +0400

    Vue files where changed to vue.cli

---
+++ vue-methods/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <BlogArticles />
+  </div>
+</template>
+
+<script>
+import BlogArticles from './components/BlogArticles.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BlogArticles
+  }
+}
+</script>
+
+<style>
+
+</style>
